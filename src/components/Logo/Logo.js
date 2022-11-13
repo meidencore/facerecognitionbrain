88176d64951f542c5ce4350d2837d5b0774f5fc0
Meidencore <danieleverdale@gmail.com>
2022-11-13 11:03:55 -0500

    create components Logo and Navigations

---
+++ src/components/Logo/Logo.js
@@ -0,0 +1,18 @@
+import React from "react";
+import Tilt from 'react-parallax-tilt';
+import brain from './brain.png'
+import './Logo.css'
+
+const Logo = () => {
+    return (
+    <div className="ma4 mt0">
+        <Tilt className="Tilt br2 shadow-2" tiltMaxAngleX={45} tiltMaxAngleY={45}>
+            <div className="pa3">
+                <img alt="logo" src={brain}/>
+            </div>
+        </Tilt>
+    </div>
+    )
+}
+
+export default Logo
